663f8a5bc32810274d6c60d4d15b066fff3701d4
Andrew Freiburger <Andrewefreiburger@gmail.com>
2022-03-08 00:21:27 -0500

    update the setup file

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ setup(
 	'test':['databases/*', 'protein_sequence.fasta'],
     'chemw':['amino_acids_masses.json'],
   },
-  version = '0.2.4',
+  version = '0.3.0',
   license = 'MIT',
   description = "Calculates the Molecular Weight, to the appropriate significant digits, from a string of an arbitrary chemical formula, a protein sequence of one- or three-letter codes, or chemical common names that are recognized by PubChem.", 
   long_description = readme,
   author = 'Andrew Freiburger',               
   author_email = 'andrewfreiburger@gmail.com',
   url = 'https://github.com/freiburgermsu/chemw',   
-  keywords = ['chemistry', 'math', 'mass', 'weight', 'PHREEQC', 'molecular', 'mineral', 'formula', 'calculate'],
-  install_requires = ['chemicals', 'pandas', 'pubchempy', 'requests']
+  keywords = ['chemistry', 'math', 'mass', 'weight', 'PHREEQC', 'molecular', 'mineral', 'formula', 'calculate', 'significant', 'digits', 'figures', 'sigfigs', 'proteins', 'enzymes'],
+  install_requires = ['chemicals', 'pandas', 'pubchempy', 'requests', 'sigfig']
 )
